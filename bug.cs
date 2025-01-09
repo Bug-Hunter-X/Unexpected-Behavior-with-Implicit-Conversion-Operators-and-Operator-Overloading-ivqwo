f5d6b4f78d217788d04721bec3dd3eab0503e1c4
BugHunter-X <askzenai@gmail.com>
2025-01-09 18:34:06 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,31 @@
+public class MyClass
+{
+    public int MyProperty { get; set; }
+
+    public MyClass(int value)
+    {
+        MyProperty = value;
+    }
+
+    public static implicit operator int(MyClass obj)
+    {
+        return obj.MyProperty;
+    }
+
+    public static implicit operator MyClass(int value)
+    {
+        return new MyClass(value);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MyClass obj1 = 10; // Implicit conversion from int to MyClass
+        int value1 = obj1; // Implicit conversion from MyClass to int
+
+        MyClass obj2 = new MyClass(20);
+        int value2 = obj2 + 5; // Unexpected behavior: MyClass + int is not defined
+    }
+}
